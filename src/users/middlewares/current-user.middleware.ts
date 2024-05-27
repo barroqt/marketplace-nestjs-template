19d0871c949495fb3369455e5bdac14c90696a57
barroqt <barroqt@gmail.com>
2024-05-28 01:12:34 +0200

    query strings

---
+++ src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,24 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { UsersService } from '../users.service';
+import { User } from '../users.entity';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    currentUser?: User;
+  }
+}
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(private usersService: UsersService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req.session || {};
+    if (userId) {
+      const user = await this.usersService.findOne(parseInt(userId));
+      req.currentUser = user;
+    }
+    next();
+  }
+}
