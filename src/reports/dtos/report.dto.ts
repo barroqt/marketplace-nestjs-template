4d5f1ca3b9c3a5e684b249b95f61d3b3edbc8b19
barroqt <barroqt@gmail.com>
2024-05-27 22:25:37 +0200

    create reports and associate to users with typeorm

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,32 @@
+import { Expose, Transform } from 'class-transformer';
+import { User } from '../../users/users.entity';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  lng: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  mileage: number;
+
+  @Expose()
+  @Transform(({ obj }) => obj.user.id)
+  userId: number;
+}
